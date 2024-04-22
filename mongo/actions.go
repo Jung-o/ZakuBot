3b2c8a833ea2fcec0842392d6d91307125dc89d9
Hugo RAPS <hugo.raps@telecomnancy.eu>
2024-04-22 02:11:51 +0200

    [+] mongodb connection and user registration

---
+++ mongo/actions.go
@@ -0,0 +1,31 @@
+package mongo
+
+import (
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+)
+
+var client = NewMongoClient()
+var db = client.Database("ZakuBot")
+var usersColl = db.Collection("Users")
+var inventoriesColl = db.Collection("Inventories")
+var cardsColl = db.Collection("Cards")
+
+func registerUser(userID string) string {
+	userDoc := bson.M{"userID": userID}
+	err := usersColl.FindOne(ctx, userDoc).Decode(&userDoc)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			_, err := usersColl.InsertOne(ctx, userDoc)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return "User registered successfully"
+		} else {
+			log.Fatal(err)
+		}
+	}
+	return "User already exists"
+}
---
+++ mongo/client.go
@@ -0,0 +1,25 @@
+package mongo
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+)
+
+var ctx = context.TODO()
+
+func NewMongoClient() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
