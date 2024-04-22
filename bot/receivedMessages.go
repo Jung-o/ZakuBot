9d080a9e805cdff6ba1d0dfff47fc4202cc7b9db
Hugo R <hugo.raps02@gmail.com>
2024-04-22 06:09:05 +0200

    [+] added draw random cards ability

--- bot/receivedMessages.go
+++ bot/receivedMessages.go
@@ -1,8 +1,18 @@
 package bot
 
-import "ZakuBot/mongo"
+import (
+	"ZakuBot/mongo"
+)
 
 func register(userID string) string {
 	status := mongo.RegisterUser(userID)
 	return status
+}
+
+func drawCards() (string, error) {
+	cards, err := mongo.DrawCards()
+	if err != nil {
+		return "", err
+	}
+	return CombineDrawnCards(cards), nil
 }
